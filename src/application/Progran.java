bcfba5261923f852e3318004a65e6d9922d217ca
“Denis <denis.wilsom.munerolli@gmail.com>
2024-06-27 14:28:10 -0300

    Department entities class
    implementação de classe Department
    atributos
    costructor
    get/ set
    hashCode and equals
    toString

---
+++ src/application/Progran.java
@@ -0,0 +1,14 @@
+package application;
+
+import model_entities.Department;
+
+public class Progran {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
