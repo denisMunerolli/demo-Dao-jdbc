4507920a747607881e5e432192ad696d8e3271f2
“Denis <denis.wilsom.munerolli@gmail.com>
2024-06-28 12:23:40 -0300

    Reusing Instantiation
    reorganizei as instanciaçoes para que o codigo ficasse mais organizado e legivel

--- src/application/Progran.java
+++ src/application/Progran.java
@@ -13,6 +13,9 @@ public class Progran {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao(); 
 		
+		
+		System.out.println("=== TEST 1: seller findById ====");
+		
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
