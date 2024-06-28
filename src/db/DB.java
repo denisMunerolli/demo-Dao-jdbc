e3c532eda340120f392274a63632728dc75e699e
“Denis <denis.wilsom.munerolli@gmail.com>
2024-06-27 22:20:08 -0300

    findById implementation
    sem comentarios nesse momento

--- src/db/DB.java
+++ src/db/DB.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -51,6 +52,11 @@ public class DB {
 	}
 
 	public static void closeStatement(PreparedStatement st) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void closeResultSet(ResultSet rs) {
 		// TODO Auto-generated method stub
 		
 	}
