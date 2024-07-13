728d6b95898c8b911afc6d6913a56ea9c33d9b5b
“Denis <denis.wilsom.munerolli@gmail.com>
2024-07-12 21:59:14 -0300

    Insert implementation

--- src/application/Progran.java
+++ src/application/Progran.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -36,7 +37,11 @@ public class Progran {
 	    for (Seller obj : list) {
 	        System.out.println(obj);
 	    }
-		
+	    
+	    System.out.println("\n=== TEST 4: seller insert ====");
+	    Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+	    sellerDao.insert(newSeller);
+	    System.out.println("Insertd! New id = " + newSeller.getId());
 //		DB.closeConnection();
 
 	}
